92445529bcfa9e0bd08376cf9c811f7a407bd45c
Lakshman <lakshman1122000@gmail.com>
2025-09-18 18:50:06 +0530

    commited

--- nodes/planner.py
+++ nodes/planner.py
@@ -1,5 +1,5 @@
 from typing import Dict, Any, List
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
 
 import os
